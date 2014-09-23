39ee1dfa70f9b85411699ffbae2e1e281c86caeb
Sean Champ <gimbal+github@metacommunity.info>
2014-09-23 13:07:27 -0700

    revisisions in FIND-COMPONENT; rename properties container=>location

--- src/main/cltl/asdf-utils.lisp
+++ src/main/cltl/asdf-utils.lisp
@@ -15,54 +15,80 @@
     :initarg :component
     :accessor component-condition-component)))
 
-(define-condition component-container-condition ()
-  ((container
-    :initarg :container
-    :accessor component-container-condition-container)))
+(define-condition component-location-condition ()
+  ((location
+    :initarg :location
+    :accessor component-location-condition-location)))
 
-(define-condition module-component-absent (program-error
-					   component-condition
-					   component-container-condition)
+(define-condition component-not-found (program-error
+				       component-condition
+				       component-location-condition)
   ()
-  (:report (lambda (c s)
-	     (format s "Module ~S does not contain a component ~S"
-		     (component-container-condition-container c)
-		     (component-condition-component c)))))
+  (:report
+   (lambda (c s)
+     (format s "Location ~S does not contain a component ~S"
+	     (component-location-condition-location c)
+	     (component-condition-component c)))))
 
-;; (error 'module-component-absent :component "FOO" :container "BAR")
+;; (error 'component-not-found :component "FOO" :location "BAR")
 
 (define-condition system-not-found (program-error
 				    component-condition
-				    component-container-condition)
+				    component-location-condition)
   ()
-  (:default-initargs :container asdf::*source-registry*)
-  (:report (lambda (c s)
-	     (format s "Source registry ~S does not contain a system ~S"
-		     (component-container-condition-container c)
-		     (component-condition-component c)))))
+  (:default-initargs :location asdf::*source-registry*)
+  (:report
+   (lambda (c s)
+     (format s "Source registry ~S does not contain a system ~S"
+	     (component-location-condition-location c)
+	     (component-condition-component c)))))
 
 
 ;;; * Interfaces onto ASDF Component Search
+(macrolet ((frob-c (component location errp)
+	     (with-gensym (%c %comp %loc %ep)
+	       `(let* ((,%comp ,component)
+		       (,%loc ,location)
+		       (,%ep ,errp)
+		       (,%c  (find-component ,%loc ,%comp)))
+		  (cond
+		    (,%c (values ,%c))
+		    (,%ep (error 'module-component-absent
+				 :location ,%loc
+				 :component ,%comp))
+		    (t (values nil)))))))
 
-(defgeneric find-component* (component context &optional errorp)
-  ;; FIXME: The order of arguments in this function
-  ;; differs substantially from that in ASDF:FIND-COMPONENT
-  (:method (system (context null) &optional (errorp t))
-    (let ((s (find-system system nil)))
-      (cond
-	(s (values s))
-	(errorp (error 'system-not-found :component system))
-	(t (values nil)))))
-  (:method (component (context module) &optional (errorp t))
-    (let ((c  (find-component context component)))
-      (cond
-	(c (values c))
-	(errorp (error 'module-component-absent
-		       :container context
-		       :component component))
-	(t (values nil))))))
+  (defgeneric find-component* (component location &optional errorp)
+    ;; NOTE: The order of arguments in this function
+    ;; differs substantially from that in ASDF:FIND-COMPONENT
+
+    (:method (component (path cons) &optional (errorp t))
+      (frob-c component path errorp))
+
+    (:method (component (path module) &optional (errorp t))
+      (frob-c component path errorp))
+
+    (:method (component (location string) &optional (errorp t))
+      (let ((sys (find-component* location nil errorp)))
+	(when sys 
+	  (frob-c component sys errorp))))
+
+    (:method (system (location null) &optional (errorp t))
+      (let ((s (find-system system nil)))
+	(cond
+	  (s (values s))
+	  (errorp (error 'system-not-found :component system))
+	  (t (values nil)))))))
+
+;;; Test forms
 
 ;; (find-component* '#:foobarquux nil)
+;;
+;; (find-component* "asdf-utils" "info.metacommunity.cltl.utils")
+;;
+;; (find-component* "line" '("garnet-bitmaps" "garnetdraw"))
+
+
 
 (defun find-system* (name &optional (errorp t))
   (declare (type component-designator name)
