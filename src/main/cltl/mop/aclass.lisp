69f91ec7b85337dd62bc751fe66963317a5dd1fc
Sean Champ <gimbal+github@metacommunity.info>
2014-12-30 10:59:52 -0800

    rename accessors on ASSOCIATVE-INDEX; define SIMPLE-ASSOCIATVE-INDEX; define COMPUTE-KEY; redefine generic functions and methods, specializing onto ASSOCIATIVE-INDEX

--- src/main/cltl/mop/aclass.lisp
+++ src/main/cltl/mop/aclass.lisp
@@ -1,72 +1,98 @@
-;; aclass.lisp - definition of associative-class
+;; aclass.lisp - protocol for generic tabular indexing onto CLOS
 
 (in-package #:info.metacommunity.cltl.utils.mop)
 
 
-(defgeneric class-object-table-lock (class))
-(defgeneric (setf class-object-table-lock) (new-value class))
+;;; % Associative Index
 
-(defgeneric class-object-table (class))
-(defgeneric (setf class-object-table) (new-value class))
+(defgeneric object-table-lock (index))
+(defgeneric (setf object-table-lock) (new-value index))
 
-(defgeneric class-object-key-slot (class))
-(defgeneric (setf class-object-key-slot) (new-value class))
+(defgeneric object-table (index))
+(defgeneric (setf object-table) (new-value index))
 
-(defclass associative-class (standard-class)
+(defgeneric object-key-slot (index))
+(defgeneric (setf object-key-slot) (new-value index))
+
+(defclass associative-index ()
   ((object-table-lock
     :initarg :object-table-lock
-    ;; :type lock ;; TO DO : Define this type in bordeaux-threads
-    :accessor class-object-table-lock)
+    ;; :type lock ;; FIXME: Define type `LOCK' in bordeaux-threads
+    :accessor object-table-lock)
    (object-table
+    ;; FIXME: This slot should be defined in a subclass,
+    ;;  as to not require that every ASSOCIATIVE-INDEX would store a
+    ;;  HASH-TABLE
     :initarg :object-table
-    :accessor class-object-table
+    :accessor object-table
     :type hash-table
     :initform (make-hash-table :test 'eq))
-   (key-slot
-    :initarg :key-slot
-    :accessor class-object-key-slot
-    :type symbol
-    )))
+   ))
 
-(validate-class associative-class)
 
-(defmethod shared-initialize :after ((instance associative-class)
+(defmethod shared-initialize :after ((instance associative-index)
                                      slots &rest initargs)
   (declare (ignore initargs))
   (when (or (eq slots t)
             (find 'object-table-lock (the list slots) :test #'eq))
-    (setf (class-object-table-lock instance)
+    (setf (object-table-lock instance)
           (make-lock (format nil "Object-Table Lock (~S)"
                              (or (ignore-errors (class-name instance))
                                  instance))))))
 
-(defmethod finalize-inheritance :after ((class associative-class))
-  (let ((ks (class-object-key-slot class))
-        (sl (class-slots class)))
-    (unless (find ks (the list sl)
-                  :key #'slot-definition-name
-                  :test #'eq )
-      (simple-style-warning  "Key slot ~S not defined to class ~S" 
-                             sl class))))
 
-(defgeneric register-object (object class)
-  (:method ((object standard-object) (class associative-class))
-    (with-lock-held ((class-object-table-lock class))
-      (let* ((ks (class-object-key-slot class))
-             (key (slot-value object ks))
-             (table (class-object-table class))
+;;; % Simple Associative Index
+
+(defgeneric object-table-key-function (index))
+(defgeneric (setf object-table-key-function) (new-value index))
+
+(defclass simple-associative-index (associative-index)
+  ((key-function
+    :initarg :key-function
+    :type function
+    :accessor object-table-key-function)))
+
+
+(defmethod shared-initialize :around ((instance simple-associative-index)
+                                      slots &rest initargs
+                                      &key (key-function nil kfp))
+  (let (args-changed-p)
+    (when (and kfp
+               (not (typep key-function 'function)))
+      (setf args-changed-p t
+            (getf initargs :key-function)
+            (coerce key-function 'function)))
+    (cond
+      (args-changed-p
+       (apply #'call-next-method instance slots initargs))
+      (t (call-next-method)))))
+
+
+;;; % Index Protocol
+
+(defgeneric compute-key (object index)
+  (:method (object (index simple-associative-index))
+    (funcall (the function (object-table-key-function index))
+             object)))
+
+
+(defgeneric register-object (object index)
+  (:method ((object standard-object) (index associative-index))
+    (with-lock-held ((object-table-lock index))
+      (let* ((key (compute-key object index))
+             (table (object-table index))
              (existing (gethash key table)))
         (when existing
-          (warn 'redefinition-condition 
+          (warn 'redefinition-condition
                 :new object
                 :previous existing))
         (setf (gethash key table) object)
         (values object key)))))
 
-(defgeneric find-object (name class &optional errorp)
-  (:method (name (class associative-class) &optional (errorp t))
-    (with-lock-held ((class-object-table-lock class))
-      (let ((table (class-object-table class)))
+(defgeneric find-object (name index &optional errorp)
+  (:method (name (index associative-index) &optional (errorp t))
+    (with-lock-held ((object-table-lock index))
+      (let ((table (object-table index)))
         (multiple-value-bind (instance foundp)
             (gethash name table)
           (cond
@@ -74,25 +100,49 @@
             (errorp (error 'entity-not-found :name name))
             (t (values nil nil))))))))
 
-(defgeneric remove-object (name class)
-  (:method (name (class associative-class))
-    (with-lock-held ((class-object-table-lock class))
-      (let* ((table (class-object-table class))
+(defgeneric remove-object (name index)
+  (:method (name (index associative-index))
+    (with-lock-held ((object-table-lock index))
+      (let* ((table (object-table index))
              (exp (remhash name table)))
         (values exp name)))))
 
-(defgeneric map-objects (function class)
-  (:method (function (class associative-class))
-    (map-objects (coerce function 'function) class))
-  (:method ((function function) (class associative-class))
-    (with-lock-held ((class-object-table-lock class))
-      (let ((table (class-object-table class)))
+(defgeneric map-objects (function index)
+  (:method (function (index associative-index))
+    (map-objects (coerce function 'function) index))
+  (:method ((function function) (index associative-index))
+    (with-lock-held ((object-table-lock index))
+      (let ((table (object-table index)))
         (labels ((fncall (name object)
                    (declare (ignore name))
                    (funcall function object)))
           (maphash #'fncall table)))
-      (values class))))
+      (values index))))
 
+
+;;; % Associative Class
+
+(defclass associative-class (associative-index standard-class)
+  ((key-slot
+    :initarg :key-slot
+    :accessor object-key-slot
+    :type symbol
+    )))
+
+(validate-class associative-class)
+
+
+(defmethod finalize-inheritance :after ((class associative-class))
+  (let ((ks (object-key-slot class))
+        (sl (class-slots class)))
+    (unless (find ks (the list sl)
+                  :key #'slot-definition-name
+                  :test #'eq )
+      (simple-style-warning  "Key slot ~S not defined to class ~S"
+                             sl class))))
+
+(defmethod compute-key ((object standard-object) (index associative-class))
+  (slot-value object (object-key-slot index)))
 
 ;; (defgeneric remove-object ...)
 
@@ -110,14 +160,14 @@
       (af2 (make-instance 'afoo :fie '|2| :fum "Two"))
       (c (find-class 'afoo))
       #+NIL (af3 (make-instance 'afoo)))
-  (register-object af1 c) 
-  (register-object af2 c) 
+  (register-object af1 c)
+  (register-object af2 c)
   (labels ((frob-test (k)
              (let ((o (find-object k c)))
                (eq (slot-value o 'fie) k))))
     (values (frob-test '|1|)
             (frob-test '|2|))))
 ;;; => T, T
-  
+
 
 |#
