c59f4585fce0401edda1c1dc78242da9e44f9134
Sean Champ <spchamp@users.noreply.github.com>
2018-11-11 15:02:46 -0800

    Update source example text for cosmetic renames in ASDF 3

--- src/main/cltl/utils/print-utils.lisp
+++ src/main/cltl/utils/print-utils.lisp
@@ -299,7 +299,7 @@ If AT-P is non-nil, PRINC to STREAM: the value of OBJECT-PRINT-LABEL applied ont
              (format stream format-control k v))
            table))
 
-;; (with-output-to-string (*standard-output*) (print-hash-table asdf::*defined-systems*))
+;; (with-output-to-string (*standard-output*) (print-hash-table asdf/system-registry:*registered-systems*))
 
 (defgeneric object-name (object))
 (defgeneric (setf object-name) (new-value object))
