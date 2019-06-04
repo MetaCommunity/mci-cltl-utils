928b0932bf9c01be66e513b4b3e82a45244647d4
Sean Champ <spchamp@users.noreply.github.com>
2019-06-04 14:08:01 -0700

    Update annotations (Remarks - API Design, Implementation)

--- src/main/lsp/mop/singleton/singleton.lisp
+++ src/main/lsp/mop/singleton/singleton.lisp
@@ -1,4 +1,4 @@
-;; singleton.lisp
+;; singleton.lisp - ltp-common-mop-singleton protocol definitions
 ;;------------------------------------------------------------------------------
 ;;
 ;; Copyright (c) 2019 Sean Champ and others. All rights reserved.
@@ -23,6 +23,7 @@
 (in-package #:ltp/common/mop/singleton)
 
 
+;; -- Singleton Class Definition
 
 (defclass singleton (standard-class)
   ())
@@ -31,8 +32,14 @@
 (validate-class singleton)
 
 
+;; FIXME: Automatically finalize SINGLETON after initialization.
+;;
+;; (NB: SINGLETON is not a forward-referenced class)
+
+
+;; -- Singleton Slot Definitions
+
 (defclass singleton-slot-definition (standard-slot-definition)
-  ;; TBD: SLOT-DEFINITION (??)
   ()
   (:default-initargs
    :allocation :class))
@@ -41,9 +48,14 @@
 (defclass singleton-direct-slot-definition
     (singleton-slot-definition standard-direct-slot-definition)
   ;; NB: These will extend the standard MOP classes
-  ;; though the allocation meta-slot will be by-in-large
-  ;; unused here - assumed to be, in effect, always :CLASS
+  ;;
+  ;; The allocation meta-slot will be by-in-large unused here - assumed
+  ;; to be, in effect, always :CLASS
   ())
+
+;; FIXME: Err for any singleton slot definition with
+;; :ALLOCATION :INSTANCE specified during slot metaobject
+;; initialization
 
 
 (defmethod direct-slot-definition-class ((class singleton)
@@ -63,18 +75,47 @@
 
 ;; MOP Interop ...
 
+;; --
+
 ;; Applications TD
 ;; - Use for internal representation of "Application Personalities" in
-;;   the Lisp Environment
+;;   the Lisp Environment - Class definitions, method definitions, and
+;;   runtime application protocols.
 ;;
-;; - Use for internal representation of ABIs
+;; - Use for internal representation of bytecode ABIs (host ABIs,
+;;   and toolchain ABIs)
+;;
+;; - Extend SINGLETON => SINGLETON-FUNCALLABLE-INSTANCE
+;;
+;;   - Use in definitions of external API forms - onto external type
+;;     schema, as also extending SINGLETON)
+;;
+;;   - Observe XMI as a serialized representation of MOF (subsq. UML and
+;;     other metamodels tractably extending OMG MOF) towards
+;;     consideration with regards to portable definitions for such type
+;;     schema. (NB: MOF and RDF may occupy different domains, per se.
+;;     Note, however, the perhaps visually-oriented syntax of the RDF
+;;     metamodel defined in OMG ODM)
+;;
+;;   - Note that this is not, per se, for redefining any single manner
+;;     of knowledge representation system
 
-;; NB: Static dispatching w/ CLtL2+MOP standard methods
+;; TBD: SINGLETON-FUNCALLABLE-INSTANCE & CLASS-OWNED-METHOD definitions
+;;
+;; - NB: Common Lisp GENERIC-FUNCTION metaobjects as globally scoped
+;;       objects, principally extending on a semantics of the Lisp
+;;       function as a first order object, for purposes of definining
+;;       polymorphic functions in Common Lisp programs
+;;
+;; - NB: Polymorphic functions in Simula and subsq.
+;;
+;; - NB: Type signatures in SML
 
 
-;; FIXME: Err for any singleton slot definition with
-;; :allocation :instance specified during metaobject
-;; initialization
+;; TBD: SINGLETON usage in _static dispatching_ w/ portable extensions
+;; onto CLtL2+MOP standard generic functions and standard methods - at
+;; least insofar as may be portably integrated with any number of PCL
+;; implementations.
 
 #+nil
 (eval-when ()
