7eec78ee6501dacb3c2f616b12a0f4d9a3cc94de
Sean Champ <spchamp@users.noreply.github.com>
2019-06-25 15:33:05 -0700

    Define LEAST-COMMON-SUPERCLASS [Function], INSTANCE [Type]

--- src/main/lsp/mop/common/mop-utils.lisp
+++ src/main/lsp/mop/common/mop-utils.lisp
@@ -28,3 +28,81 @@
 in this implementation -~> ~<class ~s with superclass ~s~>"
       (quote ,class) (quote ,superclass))
      (values)))
+
+
+;; -- Other Utils
+
+;; ---- Class Precedence Reflection
+
+(declaim (ftype (function (class-designator class-designator)
+                          (values class cons &optional))
+                least-common-superclass))
+
+(defun least-common-superclass (a b)
+  ;; NB: MOP-util Onto CLOS, per se
+  (let* ((%a (compute-class a))
+         (%b (compute-class b))
+         (common (nreverse
+                  (the cons
+                       (intersection (the cons (class-precedence-list %a))
+                                     (the cons (class-precedence-list %b))
+                                     :test #'eq)))))
+    (declare (type class %a %b)
+             (type cons common))
+    (values (car common) common)))
+
+;; (least-common-superclass 'standard-object 'structure-object)
+
+
+;; ---- Portable INSTANCE (not CONDITION) alias type
+
+(deftype instance ()
+  (load-time-value (class-name
+                    #+NIL
+                    (least-common-superclass
+                     (find-class 'condition)
+                     (least-common-superclass 'standard-object
+                                              'structure-object))
+                    #-NIL
+                    (least-common-superclass 'standard-object
+                                             'structure-object))
+                   t))
+
+;; (typep (class-prototype (find-class 'standard-class )) 'instance)
+;; => T
+
+;; (typep (class-prototype (find-class 'structure-class )) 'instance)
+;; => T
+
+;; QA - INSTANCE deftype & [SBCL]
+;;
+;; (typep (class-prototype (find-class 'condition)) 'sb-pcl::slot-object)
+;; => NIL
+;;
+;; Subsq ...
+;;
+;; (typep (class-prototype (find-class 'condition)) 'instance)
+;; => NIL
+;;
+;; NB [SBCL]
+;;    (class-precedence-list (find-class 'condition))
+;; ... which may seem inconsistent onto the above TYPEP calls.
+;;
+;; Subsq ...
+;;
+;; [Indemnification Clause Here]
+
+#+NIL
+(eval-when ()
+  ;; QA - test the INSTANCE type for consistency onto an initialized
+  ;; CONDITION object
+  ;;
+  ;; - tested with SBCL 1.4.16.debian
+  ;;   - TYPEP CONDITION 'INSTANCE => NIL
+  (handler-bind ((condition (lambda (c)
+                              (warn "FROB: ~S ~S"
+                                    (typep c 'instance)
+                                    (class-of c)))))
+    (signal 'condition))
+
+)
